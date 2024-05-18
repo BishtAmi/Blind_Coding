77f9c61ad8588644176227eb9842a4cfd815769e
Amit_Bisht <singhbishtamit2@gmail.com>
2024-05-18 16:47:21 +0530

    ide added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,40 @@
+"use client"
+import { useEffect, useState } from "react";
 import Navbar from "./navbar/page";
 import Image from "next/image";
 import Home from "../public/Homepage.png"; // Import Image component from Next.js
 
 const HomePage = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div>
-        <Image src={Home} alt="Main Image"  width={1300} />
+        <Image
+          src={Home}
+          alt="Main Image"
+          width={windowSize.width}
+          height={windowSize.height}
+        />
       </div>
 
       {/* Other content */}
