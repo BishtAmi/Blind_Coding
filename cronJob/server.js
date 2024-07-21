4e24489a364cb246bce7456c8ef36800dd06c13b
Amit_Bisht <singhbishtamit2@gmail.com>
2024-07-21 22:24:35 +0530

    automatic DB updated

--- cronJob/server.js
+++ cronJob/server.js
@@ -23,7 +23,7 @@ cron.schedule(
     timezone: "Asia/Kolkata", // Indian time
   }
 );
-
-app.listen(8000, () => {
-  console.log(`Listening on port ${8000}`);
+const PORT = 10000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
